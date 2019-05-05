9b593f5e24f638684a06aeed64c498b04b631269
Karine Araujo <pires.karine6@gmail.com>
2019-05-05 19:04:30 -0300

    inicio da implementacao de predios.py

---
+++ credenciados.py
@@ -0,0 +1,25 @@
+import pessoa as Pessoa
+import predio as Predio
+import andar as Andar
+
+##
+# ID das Pessoas credenciadas: 2, 5, 7
+# ID dos Prédios: 1, 2
+# ID dos Andares: 1, 2, 3, 4, 5
+##
+
+##
+# Especificação dos acessos:
+# Pessoa 2 tem acesso ao prédio 1 e aos andares 2, 4
+# Pessoa 5 tem acesso ao prédio 2 e aos andares 3, 5
+# Pessoa 7 tem acesso ao prédio 1 e aos andares 1, 4 e 5 e ao prédio 2 com acesso ao andar 3
+##
+class Credenciados:
+    def retornaListaCredenciados():
+        lista_credenciados = []                     #lista provisória de credenciados no complexo
+        
+        lista_credenciados.append(Pessoa.Pessoa("2", [Predio.Predio("1", [Andar.Andar("2"), Andar.Andar("4")])]))
+        lista_credenciados.append(Pessoa.Pessoa("5", [Predio.Predio("2", [Andar.Andar("3"), Andar.Andar("5")])]))
+        lista_credenciados.append(Pessoa.Pessoa("7", [Predio.Predio("1", [Andar.Andar("1"), Andar.Andar("4"), Andar.Andar("5")]), Predio.Predio("2", [Andar.Andar("3")])]))
+
+        return lista_credenciados
--- predios.py
+++ predios.py
@@ -5,6 +5,7 @@ import time
 import zmq
 import sys
 
+lista_credenciados = Credenciados.Credenciados.retornaListaCredenciados()
 
 porta = sys.argv[1]                                     #Recebe a porta como argumento na hora da execução do código
 
@@ -12,22 +13,27 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:"+ porta)                          #Socket faz a conexão com o servidor
 
-lista_credenciados = ("2", "5", "7")                    #lista provisória de credenciados no predio
-
 while True:
     print("Esperando requisição")
     requisicao = socket.recv_string()                   #Recebe a mensagem do cliente
-    id_requisicao, visitante = requisicao.split()       #Tratamento da mensagem
+    id_requisicao, id_predio_solicitado, visitante = requisicao.split()       #Tratamento da mensagem
 
     print("CLIENTE: %s" %id_requisicao)                 #informação da requisição será exibida do servidor
     print("CREDENCIAL: %s" % visitante)
 
-    if (id_requisicao in lista_credenciados) and (visitante == "cadastrado"):       #Tratamento das requisições
-        socket.send_string("Cliente %s permitido e credenciado" % id_requisicao)    #caso de cliente na lista do predio e credenciado
+    flagAutorizadoNoPredio = False
+    for credenciado in lista_credenciados:
+        if(credenciado.id == id_requisicao):
+            for predio in credenciado.listaPredios:
+                if(predio.id == id_predio_solicitado):
+                    flagAutorizadoNoPredio = True
+                    break
+            if(flagAutorizadoNoPredio):
+                break;
+
+    if (flagAutorizadoNoPredio):       #Tratamento das requisições
+        socket.send_string("Cliente %s permitido no prédio solicitado" % id_requisicao)    #caso de cliente na lista do predio e credenciado
     else:
-        if visitante == "visitante":                                                #caso de cliente visitando o predio
-            socket.send_string("Cliente %s permitido como visitante"% id_requisicao)
-        else:                                                                       #caso o cliente informe que é credenciado e não consta na lista
-            socket.send_string("Cliente nao permitido!")
+        socket.send_string("Cliente nao permitido!")
 
     time.sleep(1)
